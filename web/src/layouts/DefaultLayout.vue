4397d1ace90e3b24cd17f1bb154b2a2bc6d50500
aquaswim <bayusetiawan37@gmail.com>
2025-08-05 09:14:15 +0700

    progress: router

---
+++ web/src/layouts/DefaultLayout.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style scoped></style>

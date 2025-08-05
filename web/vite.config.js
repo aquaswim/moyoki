1e2ce2ad1eca91cfdb0c923be7a37f51caf5a572
aquaswim <bayusetiawan37@gmail.com>
2025-08-05 22:29:54 +0700

    chore: add tailwind

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import devManifest from "vite-plugin-dev-manifest";
 import eslint from "vite-plugin-eslint";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 5173,
   },
-  plugins: [eslint(), vue(), devManifest()],
+  plugins: [eslint(), vue(), devManifest(), tailwindcss()],
   build: {
     manifest: true,
     rollupOptions: {

2ccbffc59b4df9afa110bc3702a70427d97476a0
aquaswim <bayusetiawan37@gmail.com>
2025-10-16 22:32:01 +0700

    chore: not important

--- web/src/pages/AboutPage.vue
+++ web/src/pages/AboutPage.vue
@@ -1,7 +1,14 @@
 <script setup></script>
 
 <template>
-  <h1>About</h1>
+  <div class="container mx-auto">
+    <div class="flex justify-between items-center mb-6">
+      <h1 class="text-2xl font-bold">About</h1>
+    </div>
+    <div>
+      <p>Someday i will add something here!</p>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
